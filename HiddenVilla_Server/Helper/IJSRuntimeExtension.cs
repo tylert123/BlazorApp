d74e1256aa20a1b9e1064653d62c043bb73ceb9a
Tyler <torrest123@gmail.com>
2022-02-20 14:05:55 -0600

    Blazor section 6 complete

---
+++ HiddenVilla_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+
+namespace HiddenVilla_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async ValueTask ToastrError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+        public static async ValueTask SweetSuccess(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowSweetAlert", "success", message);
+        }
+        public static async ValueTask SweetError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowSweetAlert", "error", message);
+        }
+    }
+}
---
+++ HiddenVilla_Server/wwwroot/js/common.js
@@ -0,0 +1,26 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful");
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed");
+    }
+}
+
+window.ShowSweetAlert = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            footer: '<a href="">Why do I have this issue?</a>'
+        })
+    }
+}
