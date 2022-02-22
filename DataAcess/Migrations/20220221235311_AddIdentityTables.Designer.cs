e2dc3d127cd1a6c3ca5a4cca8ffe8e2bf74ce30c
Tyler <torrest123@gmail.com>
2022-02-22 10:17:22 -0600

    Blazor section 9 complete -- added sign in and initialized an Admin

--- DataAcess/Migrations/20220221235311_AddIdentityTables.Designer.cs
+++ DataAcess/Migrations/20220221235311_AddIdentityTables.Designer.cs
@@ -4,6 +4,7 @@ using DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAcess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220221235311_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HiddenVilla_Server/Service/DbInitializer.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+using DataAcess.Data;
+using HiddenVilla_Server.Service.IService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HiddenVilla_Server.Service
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            if (_db.Roles.Any(x => x.Name == SD.Role_Admin)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new IdentityUser
+            {
+                UserName = "tylertorres0@icloud.com",
+                Email = "tylertorres0@icloud.com",
+                EmailConfirmed = true
+            }, "@ustinT1").GetAwaiter().GetResult();
+
+            IdentityUser user = _db.Users.FirstOrDefault(u => u.Email == "tylertorres0@icloud.com");
+            _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+        }
+    }
+}
